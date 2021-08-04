8a3bc8867c463d7daa9ab281c440ca0d83bab7fc
Jaskaran Singh <karansamra@gmail.com>
2021-08-04 12:42:32 +0530

    Some good changes.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,97 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Link, NavLink, Redirect} from 'react-router-dom';
-import Route from 'react-router-dom/Route';
-import User from './components/User'
+import React, { Component } from "react";
+import { BrowserRouter as Router, NavLink, Redirect } from "react-router-dom";
+import Route from "react-router-dom/Route";
+import User from "./components/User";
 
 // const User = () => {
 //   return ("<h1>Wecome User<h1>");
 // }
 
 class App extends Component {
-
   state = {
-    loggedIn: false
-  }
+    loggedIn: false,
+  };
 
   handleLogin = () => {
     this.setState({
-      loggedIn: true
-    })
-  }
+      loggedIn: true,
+    });
+  };
+
+  handleLogout = () => {
+    this.setState({
+      loggedIn: false,
+    });
+  };
 
   render() {
     return (
       <Router>
-        <div className = "App"> 
+        <div className="App">
           <ul>
             <li>
-              <NavLink to = "/" exact activeStyle= {
-                {color: 'green'}
-              }>
+              <NavLink to="/" exact activeStyle={{ color: "green" }}>
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink to = "/about" exact activeStyle= {
-                {color: 'green'}
-              }>
+              <NavLink to="/about" exact activeStyle={{ color: "green" }}>
                 About
               </NavLink>
-            </li>  
-            <li>
-              <NavLink to = "/user/:Jaskaran" exact activeStyle= {
-                {color: 'green'}
-              }>
-                User Jaskaran
-              </NavLink>
-            </li>  
+            </li>
+            {this.state.loggedIn ? (
+              <li>
+                <NavLink
+                  to="/user/:Jaskaran"
+                  exact
+                  activeStyle={{ color: "green" }}
+                >
+                  Profile
+                </NavLink>
+              </li>
+            ) : (
+              ""
+            )}
           </ul>
 
-          <input type = "button" value = "Login" onClick={this.handleLogin.bind(this)}/>
-          <Route path="/" strict exact render = {
-            ()=>{
-              return (<h1> Welcome Home</h1>)
-            }
-          }>
-          </Route>
-          <Route path="/about" strict exact render = {
-            ()=>{
-              return (<h1> Welcome About</h1>)
-            }
-          }>
-          </Route>
-          <Route path="/user/:username" strict exact render = {
-            (props) => { return this.state.loggedIn ? <User {...props}/> : <Redirect to = "/"/> }
-          }>
-          </Route>
+          <input
+            type="button"
+            value="Login"
+            onClick={this.handleLogin.bind(this)}
+          />
+          <input
+            type="button"
+            value="Logout"
+            onClick={this.handleLogout.bind(this)}
+          />
+          <Route
+            path="/"
+            strict
+            exact
+            render={() => {
+              return <h1> Welcome Home</h1>;
+            }}
+          ></Route>
+          <Route
+            path="/about"
+            strict
+            exact
+            render={() => {
+              return <h1> Welcome About</h1>;
+            }}
+          ></Route>
+          <Route
+            path="/user/:username"
+            strict
+            exact
+            render={(props) => {
+              return this.state.loggedIn ? (
+                <User {...props} />
+              ) : (
+                <Redirect to="/" />
+              );
+            }}
+          ></Route>
         </div>
       </Router>
     );
